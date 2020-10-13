0b0fa487dc089ed282080fd8d8f0b3d100745e71
pythonian23 <63051526+pythonian23@users.noreply.github.com>
2020-10-13 14:59:56 +0900

    heroku

--- BOT/main.py
+++ BOT/main.py
@@ -21,6 +21,10 @@ client = commands.Bot(command_prefix=("^", "!"))
 
 @client.event
 async def on_ready():
+    await client.change_presence(
+        status=discord.Status.online,
+        activity=discord.Game(name="P&W")
+    )
     print("BOT READY")
 
 
