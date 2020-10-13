6d8f36d736c4807c51fdf3709860f69a5c5cb787
pythonian23 <63051526+pythonian23@users.noreply.github.com>
2020-10-13 17:22:01 +0900

    help command

--- BOT/main.py
+++ BOT/main.py
@@ -1,11 +1,17 @@
 import os
+import difflib
 import requests
 import discord
 from discord.ext import commands
 
-
 TOKEN = os.environ.get("TRANSATLANTICBOT")
 KEY = os.environ.get("PNWKEY")
+
+command_dict = {
+    "help": "Lists the commands and their descriptions",
+    "help_commands": "ALIAS: help",
+    "city": "Gets information about the specified city. URL and ID are both accepted.",
+}
 
 
 def req(loc):
@@ -16,7 +22,8 @@ def req(loc):
 
 
 # run bot
-client = commands.Bot(command_prefix=("^", "!"))
+client = commands.Bot(command_prefix=("^", "!", "also,\n", "also, "))
+client.remove_command("help")
 
 
 @client.event
@@ -41,7 +48,7 @@ async def city(ctx, *args):
                 color=discord.Color(0x00ff00)
             )
             emb.set_footer(text=f"P&W bot for ANYONE, unlike @Locutus#7602     -     Requested by {ctx.author.name}")
-            await ctx.channel.send(embed=emb)
+            await ctx.send(embed=emb)
             return
 
     theCity = req(f"city/id={c_id}")
@@ -54,7 +61,54 @@ async def city(ctx, *args):
     )
     emb.set_footer(text=f"P&W bot for ANYONE, unlike @Locutus#7602     -     Requested by {ctx.author.name}")
 
-    await ctx.channel.send(embed=emb)
+    await ctx.send(embed=emb)
     return
+
+
+@client.command(name="help", aliases=["help_commands"])
+async def help_commands(ctx, spec_command=None):
+    if spec_command is None:
+        txts = []
+        for key, val in command_dict.items():
+            txts.append(f"{key}: {val}")
+
+        joined = "\n".join(txts)
+        emb = discord.Embed(
+            title="Help",
+            description=f"```yml\n{joined}\n```",
+            color=discord.Color(0x00ff00)
+        )
+        emb.set_footer(text=f"P&W bot for ANYONE, unlike @Locutus#7602     -     Requested by {ctx.author.name}")
+
+        await ctx.send(embed=emb)
+        return
+    else:
+        if spec_command not in command_dict.keys():
+            try:
+                spec_command = difflib.get_close_matches(spec_command, command_dict.keys())[0]
+                txt = f"```yml\n{f'{spec_command}: {command_dict[spec_command]}'}\n\nCommand not found, but is this what you're looking for?\n```"
+            except IndexError:
+                emb = discord.Embed(
+                    title=f"Help - {spec_command}",
+                    description="```yml\nCommand does not exist.\n```",
+                    color=discord.Color(0x00ff00)
+                )
+                emb.set_footer(
+                    text=f"P&W bot for ANYONE, unlike @Locutus#7602     -     Requested by {ctx.author.name}")
+
+                await ctx.send(embed=emb)
+                return
+        else:
+            txt = f"```yml\n{f'{spec_command}: {command_dict[spec_command]}'}\n```"
+        emb = discord.Embed(
+            title=f"Help - {spec_command}",
+            description=txt,
+            color=discord.Color(0x00ff00)
+        )
+        emb.set_footer(text=f"P&W bot for ANYONE, unlike @Locutus#7602     -     Requested by {ctx.author.name}")
+
+        await ctx.send(embed=emb)
+        return
+
 
 client.run(TOKEN)
