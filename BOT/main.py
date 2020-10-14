5cb450c85a3944186d8592a14f70508cc4618381
pythonian23 <63051526+pythonian23@users.noreply.github.com>
2020-10-14 20:13:28 +0900

    auto update

--- BOT/main.py
+++ BOT/main.py
@@ -1,5 +1,10 @@
 import os
+import time
+import json
 import difflib
+import datetime
+import asyncio
+import threading
 import requests
 import discord
 from discord.ext import commands
@@ -14,15 +19,94 @@ command_dict = {
 }
 
 
-def req(loc):
+def req(loc, key_provider=None):
+    if key_provider is not None:
+        return requests.get(f"https://politicsandwar.com/api/{loc}{key_provider}key={KEY}").json()
     if "=" in loc:
         return requests.get(f"https://politicsandwar.com/api/{loc}&key={KEY}").json()
     else:
-        return requests.get(f"https://politicsandwar.com/api/{loc}?key={KEY}").json()
+        return requests.get(f"https://politicsandwar.com/api/{loc}/?key={KEY}").json()
+
+
+def dict_to_string(d):
+    newline = "\n"
+    return f'```yml\n{newline.join((f"{key}: {value}" for key, value in d.items()))}\n```'
+
+
+async def get_nation(ctx, n_id):
+    if not n_id.isdigit():
+        if n_id.startswith("http"):
+            n_id = n_id[n_id.find("=") + 1].rstrip("/")
+        else:
+            pass
+    return n_id
+
+
+# update
+async def update_war():
+    start_time = time.time()
+    print("\nWAR")
+    print(f"Update Started - 0 secs")
+    new = req("wars?alliance_id=7536")
+    print(f"Recieved Data - {time.time() - start_time} secs")
+    if "success" in new.keys():
+        lastcheck = datetime.datetime.utcnow() - datetime.timedelta(minutes=21)
+        wars = new["wars"]
+        war_time = datetime.datetime.strptime(wars[0]["date"][:19], "%Y-%m-%dT%X")
+        war = 0
+        while war_time >= lastcheck:
+            current_war = wars[war]
+            war_time = datetime.datetime.strptime(wars[war + 1]["date"][:19], "%Y-%m-%dT%X")
+            if current_war["attackerAA"] in ("Atlantian Council", "Atlantian Council Applicant"):
+                defense_channel = await client.fetch_channel(717536088201363496)
+            else:
+                defense_channel = await client.fetch_channel(717815077624872971)
+
+            spec_war = req(f"war/{current_war['warID']}", "/&")
+
+            emb = discord.Embed(
+                title="WAR REPORT",
+                description=dict_to_string(spec_war),
+                color=discord.Color(0x00ff00)
+            )
+            emb.set_footer(text=f"P&W bot for ANYONE, unlike SOME OTHER BOT  -  Requested by The Atlantian Council")
+
+            await defense_channel.send(embed=emb)
+            war += 1
+        print(f"Update Success - {time.time() - start_time} secs")
+    else:
+        print(f"Upate Failure - {time.time() - start_time} secs")
+    print(f"Waiting for next update - {time.time() - start_time} secs")
+
+
+async def update_nations():
+    start_time = time.time()
+    print("\nNATION")
+    print(f"Update Started - 0 secs")
+    with open("all_nations.json", "w") as nations:
+        new = req("nations")
+        print(f"Recieved Data - {time.time() - start_time} secs")
+        if "success" in new.keys():
+            json.dump(new, nations)
+            print(f"Update Success - {time.time() - start_time} secs")
+        else:
+            print(f"Upate Failure - {time.time() - start_time} secs")
+    print(f"Waiting for next update - {time.time() - start_time} secs")
+
+
+def update():
+    while True:
+        asyncio.run(update_war())
+        asyncio.run(update_nations())
+
+        time.sleep(60 * 20)
+
+
+updater = threading.Thread(target=update, daemon=True)
 
 
 # run bot
-client = commands.Bot(command_prefix=("^", "!", "also,\n", "also, "))
+client = commands.Bot(command_prefix=("^", "!", "also,\n", "also, ", "tst!"))
 client.remove_command("help")
 
 
@@ -34,10 +118,12 @@ async def on_ready():
     )
     print("BOT READY")
 
+    updater.start()
+
 
 @client.command()
-async def city(ctx, *args):
-    c_id = " ".join(args).strip(" ")
+async def city(ctx, *c_id):
+    c_id = " ".join(c_id)
     if not c_id.isdigit():
         if c_id.startswith("http"):
             c_id = c_id[c_id.find("=") + 1:].rstrip("/")
@@ -47,7 +133,8 @@ async def city(ctx, *args):
                 description=f"Your format was wrong, and I couldn't understand it. Try a link or an ID",
                 color=discord.Color(0x00ff00)
             )
-            emb.set_footer(text=f"P&W bot for ANYONE, unlike @Locutus#7602     -     Requested by {ctx.author.name}")
+            emb.set_footer(text=f"P&W bot for ANYONE, unlike SOME OTHER BOT  -  Requested by {ctx.author.name}")
+
             await ctx.send(embed=emb)
             return
 
@@ -56,13 +143,23 @@ async def city(ctx, *args):
         theCity["name"], theCity["cityid"] = "CITY NOT FOUND", c_id
     emb = discord.Embed(
         title=f"{theCity['name']} - [{theCity['cityid']}]",
-        description=f"```py\n{theCity}\n```",
+        description=dict_to_string(theCity),
         color=discord.Color(0x00ff00)
     )
-    emb.set_footer(text=f"P&W bot for ANYONE, unlike @Locutus#7602     -     Requested by {ctx.author.name}")
+    emb.set_footer(text=f"P&W bot for ANYONE, unlike SOME OTHER BOT  -  Requested by {ctx.author.name}")
 
     await ctx.send(embed=emb)
     return
+
+
+@client.command()
+async def military(ctx, n_id):
+    pass
+
+
+@client.command()
+async def depo(ctx, *args):
+    ctx.send("`!depo`? More like `!debt`")
 
 
 @client.command(name="help", aliases=["help_commands"])
@@ -78,7 +175,7 @@ async def help_commands(ctx, spec_command=None):
             description=f"```yml\n{joined}\n```",
             color=discord.Color(0x00ff00)
         )
-        emb.set_footer(text=f"P&W bot for ANYONE, unlike @Locutus#7602     -     Requested by {ctx.author.name}")
+        emb.set_footer(text=f"P&W bot for ANYONE, unlike SOME OTHER BOT  -  Requested by {ctx.author.name}")
 
         await ctx.send(embed=emb)
         return
@@ -86,7 +183,8 @@ async def help_commands(ctx, spec_command=None):
         if spec_command not in command_dict.keys():
             try:
                 spec_command = difflib.get_close_matches(spec_command, command_dict.keys())[0]
-                txt = f"```yml\n{f'{spec_command}: {command_dict[spec_command]}'}\n\nCommand not found, but is this what you're looking for?\n```"
+                txt = f"```yml\n{f'{spec_command}: {command_dict[spec_command]}'}\
+\n\nCommand not found, but is this what you're looking for?\n```"
             except IndexError:
                 emb = discord.Embed(
                     title=f"Help - {spec_command}",
@@ -94,7 +192,7 @@ async def help_commands(ctx, spec_command=None):
                     color=discord.Color(0x00ff00)
                 )
                 emb.set_footer(
-                    text=f"P&W bot for ANYONE, unlike @Locutus#7602     -     Requested by {ctx.author.name}")
+                    text=f"P&W bot for ANYONE, unlike SOME OTHER BOT  -  Requested by {ctx.author.name}")
 
                 await ctx.send(embed=emb)
                 return
@@ -105,7 +203,7 @@ async def help_commands(ctx, spec_command=None):
             description=txt,
             color=discord.Color(0x00ff00)
         )
-        emb.set_footer(text=f"P&W bot for ANYONE, unlike @Locutus#7602     -     Requested by {ctx.author.name}")
+        emb.set_footer(text=f"P&W bot for ANYONE, unlike SOME OTHER BOT  -  Requested by {ctx.author.name}")
 
         await ctx.send(embed=emb)
         return
