b3029ea8b9eeb0f1c0247a758a8cc56607ad271b
pythonian23 <63051526+pythonian23@users.noreply.github.com>
2020-10-26 10:03:36 +0900

    HelioHost

--- BOT/main.py
+++ BOT/main.py
@@ -20,6 +20,22 @@ command_dict = {
     "crypto": "🔐 MILITARY GRADE ENCRYPTION 🔐\n\tUsage: !crypto {ENC or DEC(encrypt or decrypt)} {TEXT TO CRYPT} {ID} {PASSWORD} {PSWD2 (optional)}"
 }
 
+morse = {'A': '.-', 'B': '-...',
+         'C': '-.-.', 'D': '-..', 'E': '.',
+         'F': '..-.', 'G': '--.', 'H': '....',
+         'I': '..', 'J': '.---', 'K': '-.-',
+         'L': '.-..', 'M': '--', 'N': '-.',
+         'O': '---', 'P': '.--.', 'Q': '--.-',
+         'R': '.-.', 'S': '...', 'T': '-',
+         'U': '..-', 'V': '...-', 'W': '.--',
+         'X': '-..-', 'Y': '-.--', 'Z': '--..',
+         '1': '.----', '2': '..---', '3': '...--',
+         '4': '....-', '5': '.....', '6': '-....',
+         '7': '--...', '8': '---..', '9': '----.',
+         '0': '-----', ', ': '--..--', '.': '.-.-.-',
+         '?': '..--..', '/': '-..-.', '-': '-....-',
+         '(': '-.--.', ')': '-.--.-'}
+
 
 def req(loc, key_provider=None):
     if key_provider is not None:
@@ -43,7 +59,7 @@ async def update_war():
     new = req("wars?alliance_id=7536")
     print(f"Recieved Data - {time.time() - start_time} secs")
     if "success" in new.keys():
-        lastcheck = datetime.datetime.utcnow() - datetime.timedelta(hours=0, minutes=21)
+        lastcheck = datetime.datetime.utcnow() - datetime.timedelta(hours=48, minutes=21)
         wars = new["wars"]
         war_time = datetime.datetime.strptime(wars[0]["date"][:19], "%Y-%m-%dT%X")
         curr_war = 0
@@ -57,13 +73,62 @@ async def update_war():
 
             spec_war = req(f"war/{current_war['warID']}", "/&")
 
+            attacker = req(f"nation/id={spec_war['war'][0]['aggressor_id']}")
+            defender = req(f"nation/id={spec_war['war'][0]['defender_id']}")
+
+            advantage = (
+                ("soldiers", int(attacker["soldiers"]) - int(defender["soldiers"])),
+                ("tanks", int(attacker["tanks"]) - int(defender["tanks"])),
+                ("aircraft", int(attacker["aircraft"]) - int(defender["aircraft"])),
+                ("ships", int(attacker["ships"]) - int(defender["ships"])),
+                ("missiles", int(attacker["missiles"]) - int(defender["missiles"])),
+                ("nukes", int(attacker["nukes"]) - int(defender["nukes"]))
+            )
+
+            a_adv = []
+            d_adv = []
+            n_adv = []
+
+            for adv in advantage:
+                if adv[1] > 0:
+                    a_adv.append(adv[0])
+                elif adv[1] < 0:
+                    d_adv.append(adv[0])
+                else:
+                    n_adv.append(adv[0])
+
+            advantage_txt = (
+                ", ".join(a_adv),
+                ", ".join(d_adv),
+                ", ".join(n_adv)
+            )
+
+            adv_txt = ""
+
+            if len(advantage_txt[0]):
+                adv_txt += f"The attacker is strong in {advantage_txt[0]}, "
+            else:
+                adv_txt += "The attacker has no advantages, "
+
+            if len(advantage_txt[1]):
+                adv_txt += f"while the defender is strong in {advantage_txt[1]}. "
+            else:
+                adv_txt += "while the defender has no advantages. "
+
+            if len(advantage_txt[2]):
+                adv_txt += f"The two are equal in {advantage_txt[2]}"
+
+            tab = "　" * 2
+
+            info_text = f'**{tab}[{attacker["prename"]} __{attacker["name"]}__](https://politicsandwar.com/nation/id={attacker["nationid"]}) of [__{attacker["alliance"]}__](https://politicsandwar.com/alliance/id={attacker["allianceid"]}) has attacked [{defender["prename"]} __{defender["name"]}__](https://politicsandwar.com/nation/id={defender["nationid"]}) of [__{defender["alliance"]}__](https://politicsandwar.com/alliance/id={defender["allianceid"]}) for the reason, *{spec_war["war"][0]["war_reason"]}*\n{tab}{adv_txt}**'
+
             emb = discord.Embed(
                 title="WAR REPORT",
-                description=dict_to_string(spec_war["war"][0]),
+                description=info_text,
                 color=discord.Color(0x00ff00)
             )
             emb.set_footer(
-                icon_url="https://ibb.co/0Qnh1JF",
+                icon_url="https://i.ibb.co/QXJrhmC/Atlantic-Council.png",
                 text=f"P&W bot for ANYONE, unlike SOME OTHER BOT  -  Requested by The Atlantian Council"
             )
 
@@ -120,6 +185,52 @@ async def on_ready():
     print("BOT READY")
 
 
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+
+    sarcasm = ("looks sarcastically",
+               "rolls eyes",
+               "ignores",
+               "scoffs")
+
+    if 749219251344375908 in message.raw_mentions:
+        if ("my" in message.content) and ("birthday" in message.content):
+            await message.channel.send(f"Really? Happy Birthday, <@{message.author.id}>!")
+            return
+        if "good morning" in message.content:
+            await message.channel.send(f"Thanks! Good morning, <@{message.author.id}>")
+            return
+        if "good night" in message.content:
+            await message.channel.send(f"Thanks! Sweet dreams, <@{message.author.id}>")
+            return
+        if "stfu" in message.content:
+            await message.channel.send("Uhhh... I would rather not.")
+            end = 0
+            for i in range(random.randint(2, 5)):
+                await message.channel.send(f"```cs\n#{i + 1})\n```<@{message.author.id}>")
+                end = i
+            await asyncio.sleep(5)
+            await message.author.send(f"```cs\n#{end + 2})\n```<@{message.author.id}>")
+            return
+        else:
+            await message.channel.send("**HEY!** What did you ping me for?")
+            await message.channel.send(f"<@{message.author.id}>")
+
+    if random.random() < 0.05:
+        await message.channel.send(f"*{random.choice(sarcasm)}*")
+
+
+@client.event
+async def on_typing(channel, user, _):
+    if random.random() < 0.075:
+        await channel.send(
+            f"Hey, come look at this fool, <@{user.id}>, who is typing a very **LONG** message! What a stuck-up asshole! What does he/she do better than any of us that makes him/her write a long message?")
+
+    return
+
+
 @client.command()
 async def city(ctx, *c_id):
     c_id = " ".join(c_id)
@@ -133,7 +244,7 @@ async def city(ctx, *c_id):
                 color=discord.Color(0x00ff00)
             )
             emb.set_footer(
-                icon_url="https://ibb.co/0Qnh1JF",
+                icon_url="https://i.ibb.co/QXJrhmC/Atlantic-Council.png",
                 text=f"P&W bot for ANYONE, unlike SOME OTHER BOT  -  Requested by {ctx.author.name}"
             )
 
@@ -149,7 +260,7 @@ async def city(ctx, *c_id):
         color=discord.Color(0x00ff00)
     )
     emb.set_footer(
-        icon_url="https://ibb.co/0Qnh1JF",
+        icon_url="https://i.ibb.co/QXJrhmC/Atlantic-Council.png",
         text=f"P&W bot for ANYONE, unlike SOME OTHER BOT  -  Requested by {ctx.author.name}"
     )
 
@@ -176,7 +287,7 @@ async def crypto(ctx, enc, text, iv, password, salt="Yenigma-2"):
         color=discord.Color(0x00ff00)
     )
     emb.set_footer(
-        icon_url="https://ibb.co/0Qnh1JF",
+        icon_url="https://i.ibb.co/QXJrhmC/Atlantic-Council.png",
         text=f"P&W bot for ANYONE, unlike SOME OTHER BOT  -  Requested by {ctx.author.name}"
     )
 
@@ -199,7 +310,7 @@ async def war(ctx, w_id):
         color=discord.Color(0x00ff00)
     )
     emb.set_footer(
-        icon_url="https://ibb.co/0Qnh1JF",
+        icon_url="https://i.ibb.co/QXJrhmC/Atlantic-Council.png",
         text=f"P&W bot for ANYONE, unlike SOME OTHER BOT  -  Requested by {ctx.author.name}"
     )
 
@@ -225,7 +336,7 @@ async def help_commands(ctx, spec_command=None):
             color=discord.Color(0x00ff00)
         )
         emb.set_footer(
-            icon_url="https://ibb.co/0Qnh1JF",
+            icon_url="https://i.ibb.co/QXJrhmC/Atlantic-Council.png",
             text=f"P&W bot for ANYONE, unlike SOME OTHER BOT  -  Requested by {ctx.author.name}"
         )
 
@@ -245,7 +356,7 @@ async def help_commands(ctx, spec_command=None):
                 )
                 emb.set_footer(
 
-                    icon_url="https://ibb.co/0Qnh1JF",
+                    icon_url="https://i.ibb.co/QXJrhmC/Atlantic-Council.png",
                     text=f"P&W bot for ANYONE, unlike SOME OTHER BOT  -  Requested by {ctx.author.name}"
                 )
 
@@ -259,7 +370,7 @@ async def help_commands(ctx, spec_command=None):
             color=discord.Color(0x00ff00)
         )
         emb.set_footer(
-            icon_url="https://ibb.co/0Qnh1JF",
+            icon_url="https://i.ibb.co/QXJrhmC/Atlantic-Council.png",
             text=f"P&W bot for ANYONE, unlike SOME OTHER BOT  -  Requested by {ctx.author.name}"
         )
 
